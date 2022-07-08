f88fe68f81690ed9cbc07805919161682731a2ca
Tove Backenroth <tovebac@hotmail.com>
2022-07-08 13:23:16 +0200

    code push

--- GP3_Team03.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GP3_Team03.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GP3_Team03, "GP3_Team03" );
